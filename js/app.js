3990a12c3913ce7f5fd5b306d269044146ebddf8
Shahrear-ahamed <dev.shahrear@gmail.com>
2022-02-17 12:38:01 +0600

    edit some code for clean

--- js/app.js
+++ js/app.js
@@ -3,6 +3,9 @@ function getValues(inputName){
     const inputText = document.getElementById(inputName);
     return inputAmount = parseFloat(inputText.value);
 }
+function getInnerText(findText){
+    return document.getElementById(findText);
+}
 document.getElementById("calculate-btn").addEventListener("click", function(){
     // income amount select are here
     const incomeAmount = getValues("income-input");
@@ -11,25 +14,26 @@ document.getElementById("calculate-btn").addEventListener("click", function(){
     const rentAmount = getValues("rent-input");
     const clothesAmount = getValues("clothes-input");
     // balance and total expenses 
-    const totalExpensesText = document.getElementById("total-expenses");
-    const afterBalanceText= document.getElementById("after-balance");
+    const totalExpensesText = getInnerText("total-expenses");
+    const afterBalanceText= getInnerText("after-balance");
     // total cost calculation are here
     const sumCostAmount = foodAmount+rentAmount+clothesAmount;
-    const balance = incomeAmount-sumCostAmount;
     // values 
     totalExpensesText.innerText = sumCostAmount;
-    afterBalanceText.innerText = balance;
+    afterBalanceText.innerText = incomeAmount-sumCostAmount;
 });
 
 document.getElementById("save-btn").addEventListener("click", function(){
     const incomeAmount = getValues("income-input");
     const parcentAmount = getValues("parcent");
-    const afterBalText = document.getElementById("after-balance");
-    const savingAmountText = document.getElementById("saving-amount");
-    const remainingAmountText= document.getElementById("remaining-balance");
+    // find text value
+    const afterBalText = getInnerText("after-balance");
+    const savingAmountText = getInnerText("saving-amount");
+    const remainingAmountText= getInnerText("remaining-balance");
     const afterBalAmount = parseFloat(afterBalText.innerText);
     // set values in 
     const saveparcent = incomeAmount*(parcentAmount/100);
+    
     savingAmountText.innerText = saveparcent;
     remainingAmountText.innerText = afterBalAmount-saveparcent;
 });
