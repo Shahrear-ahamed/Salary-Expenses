9256d2fcd48e13b60610ce0f393310a725914148
Shahrear-ahamed <dev.shahrear@gmail.com>
2022-02-17 16:00:43 +0600

    add error handle

--- js/app.js
+++ js/app.js
@@ -3,37 +3,54 @@ function getValues(inputName){
     const inputText = document.getElementById(inputName);
     return inputAmount = parseFloat(inputText.value);
 }
-function getInnerText(findText){
-    return document.getElementById(findText);
+function add(idOne, idTwo, constOne, constTwo){
+    const totalExpensesText = document.getElementById(idOne);
+    const afterBalanceText= document.getElementById(idTwo);
+    // values 
+    totalExpensesText.innerText = constTwo;
+    afterBalanceText.innerText = constOne-constTwo;
 }
 document.getElementById("calculate-btn").addEventListener("click", function(){
     // income amount select are here
     const incomeAmount = getValues("income-input");
-    // all cost section are here
-    const foodAmount = getValues("food-input");
-    const rentAmount = getValues("rent-input");
-    const clothesAmount = getValues("clothes-input");
-    // balance and total expenses 
-    const totalExpensesText = getInnerText("total-expenses");
-    const afterBalanceText= getInnerText("after-balance");
-    // total cost calculation are here
-    const sumCostAmount = foodAmount+rentAmount+clothesAmount;
-    // values 
-    totalExpensesText.innerText = sumCostAmount;
-    afterBalanceText.innerText = incomeAmount-sumCostAmount;
+    if(incomeAmount<0){
+        alert("Input value is negetive please input positive number")
+    }else if(isNaN(incomeAmount)){
+        alert("Input any number ");
+    }else{
+        // all cost section are here
+        const foodAmount = getValues("food-input");
+        const rentAmount = getValues("rent-input");
+        const clothesAmount = getValues("clothes-input");
+        // total cost calculation are here
+        if(foodAmount<0 && rentAmount<0 && clothesAmount<0){
+            alert("all field are negetive, and negetive value are not allow")
+        }else if(foodAmount<0 || rentAmount>0 || clothesAmount>0){
+            alert("food negetive input are not allow")
+        }else if(foodAmount>0 || rentAmount<0 || clothesAmount>0){
+            alert("rent negetive input are not allow")
+        }else if(foodAmount>0 || rentAmount>0 || clothesAmount<0){
+            alert("clothes negetive input are not allow")
+        }else{
+            const sumCostAmount = foodAmount+rentAmount+clothesAmount;
+            add("total-expenses", "after-balance", incomeAmount,sumCostAmount);
+        }
+    }
 });
 
 document.getElementById("save-btn").addEventListener("click", function(){
     const incomeAmount = getValues("income-input");
     const parcentAmount = getValues("parcent");
-    // find text value
-    const afterBalText = getInnerText("after-balance");
-    const savingAmountText = getInnerText("saving-amount");
-    const remainingAmountText= getInnerText("remaining-balance");
-    const afterBalAmount = parseFloat(afterBalText.innerText);
-    // set values in 
-    const saveparcent = incomeAmount*(parcentAmount/100);
-    
-    savingAmountText.innerText = saveparcent;
-    remainingAmountText.innerText = afterBalAmount-saveparcent;
+    if(parcentAmount<0){
+        alert("Input any positive number for in saving percent");
+    }else if(isNaN(parcentAmount)){
+        alert("please any number");
+    }else{
+        // find text value
+        const afterBalText = document.getElementById("after-balance");
+        const afterBalAmount = parseFloat(afterBalText.innerText);
+        // set values in 
+        const saveparcent = incomeAmount*(parcentAmount/100);
+        add("saving-amount", "remaining-balance", afterBalAmount, saveparcent);
+    }
 });
